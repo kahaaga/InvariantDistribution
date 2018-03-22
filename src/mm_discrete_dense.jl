dae4c485be9a33801e0b6f8aa20b65548a9e64c4
kahaaga <kahaaga@gmail.com>
2018-03-22 17:57:21 +0100

    Prefilter relevant simplices to gain some speed.

--- src/mm_discrete_dense.jl
+++ src/mm_discrete_dense.jl
@@ -2,126 +2,50 @@ using Distributions
 using InvariantDistribution
 using SimplexSplitting
 
-
 """
-Approximate the map by a Markov matrix obtained by projecting randomly
+Approximate the map by a Markov matrix.
+
+The argument `sample_how` indicates whether the approximation should be random (according
+to a Uniform distribution in the space of barycentric coordinates) or "even" in the space
+where the simplex lives (points are generated using shape-preserving splitting of simplices).
 """
-function mm_discrete_dense_test(;
-        #t::SimplexSplitting.Triangulation;
-        n_randpts::Int = 10,
-        dist::Distributions.Distribution = Distributions.Uniform(0, 1))
+function mm_discrete_dense(t::SimplexSplitting.Triangulation;
+        n_randpts::Int = 100,
+        dist::Distributions.Distribution = Distributions.Uniform(0, 1),
+        sample_randomly::Bool = false,
+        prefilter::Bool = true)
 
-    # Create example triangulation with 100 starting points
-    dim = 3
-    # Create bogus triangulation
-    n_embedding_points = 1000
-    n_simplices = 1000
-
-    embedding = rand(3, n_embedding_points)
-    points = embedding[:, 1:end-1].'
-    impoints = embedding[:, 2:end].'
-    simplex_inds = rand(1:n_embedding_points-1, n_simplices, dim+1)
-
-
-    # Create a set of convex coefficients to be used for all simplices
-    convex_coeffs = rand(dist, n_randpts, dim + 1)
-    convex_coeffs .= convex_coeffs ./ sum(convex_coeffs, 2)
-    projected_point = zeros(Float64, 1, dim) # pre-allocate
-
-    occurences = zeros(Int, n_simplices, n_simplices)
-
-    for j in 1:n_simplices
-        image_simplex = view(impoints, view(simplex_inds, j, :), :)
-        for n in 1:n_randpts
-            projected_point .= convex_coeffs[n, :].' * image_simplex
-
-            for i in 1:n_simplices
-                simplex = view(points, view(simplex_inds, i, :), :)
-                if contains_point(simplex, projected_point)
-                    occurences[j, i] += 1
-                    break
-                end
-            end
-
-        end
-    end
-
-    return occurences / n_randpts
-end
-
-
-"""
-Approximate the map by a Markov matrix obtained by projecting randomly
-"""
-function mm_discrete_dense(
-    t::SimplexSplitting.Triangulation;
-    n_randpts::Int = 100,
-    dist::Distributions.Distribution = Distributions.Uniform(0, 1))
 
     n_simplices = size(t.simplex_inds, 1)
     dim = size(t.points, 2)
 
     # Create a set of convex coefficients to be used for all simplices
-    convex_coeffs = rand(dist, n_randpts, dim + 1)
-    convex_coeffs .= convex_coeffs ./ sum(convex_coeffs, 2)
-    projected_point = zeros(Float64, 1, dim) # pre-allocate
-
-    occurences = zeros(Int, n_simplices, n_simplices)
-
-    for i in 1:n_simplices
-        image_simplex = view(t.points, view(t.simplex_inds, i, :), :)
-
-        for k in 1:n_randpts
-            @show convex_coeffs[k, :]
-            projected_point = convex_coeffs[k, :].' * image_simplex
-
-            for j in 1:n_simplices
-                simplex = view(t.points, view(t.simplex_inds, j, :), :)
-
-                if contains_point(simplex, projected_point)
-                    #println("The ", k, "th random point lies inside simplex #", j)
-                    occurences[i, j] += 1
-                    break
-                end
-            end
-
-        end
+    if sample_randomly
+        convex_coeffs = rand(dist, n_randpts, dim + 1)
+        convex_coeffs .= convex_coeffs ./ sum(convex_coeffs, 2)
+    else
+        minimum_split_factor = ceil(Int, n_randpts^(1 / dim))[1]
+        convex_coeffs = SimplexSplitting.even_sampling_rules(dim, minimum_split_factor)
     end
 
-    return occurences / n_randpts
-end
-
-"""
-Approximate the map by a Markov matrix obtained by projecting randomly
-"""
-function mm_discrete_dense_prefilter(
-    t::SimplexSplitting.Triangulation;
-    n_randpts::Int = 100,
-    dist::Distributions.Distribution = Distributions.Uniform(0, 1))
-
-    n_simplices = size(t.simplex_inds, 1)
-    dim = size(t.points, 2)
-
-    # Create a set of convex coefficients to be used for all simplices
-    convex_coeffs = rand(dist, n_randpts, dim + 1)
-    convex_coeffs .= convex_coeffs ./ sum(convex_coeffs, 2)
-    projected_point = zeros(Float64, 1, dim) # pre-allocate
-
     occurences = zeros(Int, n_simplices, n_simplices)
+    projected_point = zeros(Float64, 1, dim) # pre-allocate
 
     for i in 1:n_simplices
         image_simplex = view(t.impoints, view(t.simplex_inds, i, :), :)
 
-        # Considering the distance between centroids and radii of simplices,
-        # which simplices can possibly intersect with this image simplex?
+        if prefilter
+            inds_potentially_intersecting_simplices = potentially_intersecting_simplices(t, i)
+        else
+            inds_potentially_intersecting_simplices = 1:n_simplices
+        end
 
-        inds_potential_simplices = potentially_intersecting_simplices(t, i)
+        for k in 1:n_randpts
+            projected_point = convex_coeffs[k, :].' * image_simplex
 
-        for n in 1:n_randpts
-            projected_point .= convex_coeffs[n, :].' * image_simplex
-
-            for j in inds_potential_simplices
+            for j in potentially_intersecting_simplices
                 simplex = view(t.points, view(t.simplex_inds, j, :), :)
+
                 if contains_point(simplex, projected_point)
                     occurences[i, j] += 1
                     break
@@ -135,8 +59,10 @@ function mm_discrete_dense_prefilter(
 end
 
 
+
 """
-Approximate the map by a Markov matrix obtained by projecting randomly
+Approximate the transfer operator a Markov matrix using the information we already
+have on the orientations of the simplices.
 """
 function mm_discrete_dense_prefilter_useorientations(
     t::SimplexSplitting.Triangulation;
@@ -158,7 +84,6 @@ function mm_discrete_dense_prefilter_useorientations(
 
         # Considering the distance between centroids and radii of simplices,
         # which simplices can possibly intersect with this image simplex?
-
         inds_potential_simplices = potentially_intersecting_simplices(t, i)
 
         for n in 1:n_randpts
@@ -206,23 +131,24 @@ function contains_point(simplex::AbstractArray{Float64, 2}, point::AbstractArray
     # Keep track of signs of determinants. If all have the same sign, the point
     # lies inside the simplex. If not, it lies outside.
     signs = zeros(Float64, 4)
-
+    n_pts = size(simplex, 1)
     # Compute the first sign
-    replaced_simplex = simplex
-    replaced_simplex[1, :] = point
-    signs[1] = sign(det([replaced_simplex ones(4)]))
+    signs[1] = sign(det([vcat(point, simplex[2:end, :]) ones(n_pts)]))
 
     # Check remaining signs and stop if sign changes
-    for i = 2:size(simplex, 1)
-        replaced_simplex = simplex
-        replaced_simplex[i, :] = point
-        signs[i] = sign(det([replaced_simplex ones(4)]))
+    for i = 2:(n_pts - 1)
+        signs[i] = sign(det([vcat(simplex[1:(i - 1), :], point, simplex[(i + 1):end, :]) ones(n_pts)]))
         if !(signs[i] == signs[i-1])
             return false
         end
     end
+    signs[end] = sign(det([vcat(simplex[1:(end - 1), :], point) ones(n_pts)]))
+    if !(signs[end] == signs[end - 1])
+        return false
+    end
     return true
 end
+
 
 """
 Check whether a given `point` (1x(dim+1) array) lies inside a `simplex` ((dim+1) x dim array)
@@ -235,55 +161,11 @@ function contains_point(
         )
 
     for i = 1:size(simplex, 1)
-        replaced_simplex = simplex
+        replaced_simplex = deeopcopy(simplex)
         replaced_simplex[i, :] = point
         α = det([replaced_simplex ones(4)]) * orientation
 
         if α < 0; return false; end
     end
     return true
-end
-
-
-function childpoint(parentsimplex::Array{Float64, 2})
-    dim = size(parentsimplex, 2)
-    # Random linear combination coefficients
-    R = rand(1, dim + 1)
-
-    # Normalise the coefficients so that they sum to one. We can then create the new point
-    # as a convex linear combination of the vertices of the parent simplex.
-    normalised_coeffs = (1 ./ sum(R, 2)) .* R
-
-    normalised_coeffs * parentsimplex
-end
-
-
-function outsidepoint(parentsimplex::Array{Float64, 2})
-    dim = size(parentsimplex, 2)
-    # Random linear combination coefficients
-    R = rand(1, dim + 1)
-
-    # Normalise the coefficients so that they sum to one. We can then create the new point
-    # as a convex linear combination of the vertices of the parent simplex.
-    normalised_coeffs = (1 ./ sum(R, 2)) .* R
-    normalised_coeffs[1] += 1
-
-    normalised_coeffs * parentsimplex
-end
-
-
-function test_mm_discrete_map(t::Triangulation;
-        n_randpts = 10,
-        prefilter = true,
-        use_orientations = true,
-        split_factor = 3,
-        dim = 3)
-
-    if prefilter
-        mm_discrete_dense_prefilter(t, n_randpts = n_randpts)
-    elseif prefilter & use_orientations
-        mm_discrete_dense_prefilter_useorientations(t, n_randpts = n_randpts)
-    else
-        mm_discrete_dense(t, n_randpts = n_randpts)
-    end
 end

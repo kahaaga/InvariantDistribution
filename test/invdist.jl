05af28170a3ef8539b2496c0790b39ddb7d1e0a2
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-01 02:01:41 +0200

    Tests for discrete approximation

--- test/invdist.jl
+++ test/invdist.jl
@@ -3,7 +3,7 @@ using SimplexSplitting, InvariantDistribution
 @testset "InvDist" begin
     e_ex = Embedding(invariant_gaussian_embedding(npts = 25))
     t_ex = triang_from_embedding(e_ex)
-    mm = mm_discrete_dense(t_ex)
+    mm = mm_dd(t_ex)
     @test all(sum(mm, 2) .≈ 1)
 
     invdist = estimate_invdist(mm)

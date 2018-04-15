dbcc2086e38f9ce4b2c3e85dbb7def45cec1cc7b
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-15 17:19:48 +0200

    Transfer operator from binning

--- src/InvariantDistribution.jl
+++ src/InvariantDistribution.jl
@@ -2,11 +2,16 @@ __precompile__(true)
 
 module InvariantDistribution
 
+using GroupSlices
+
 include("invariant_maps.jl")
 include("statespace.jl")
 include("example_embeddings.jl")
 include("left_eigenvector.jl")
 include("invariantize_embedding.jl")
+
+
+# Transfer operator
 include("mm_exact/mm_sparse.jl")
 include("mm_exact/mm_sparse_parallel.jl")
 include("mm_exact/mm_parallel.jl")
@@ -14,6 +19,7 @@ include("mm_approx/mm_dd.jl")
 include("mm_approx/mm_dd3.jl")
 include("mm_approx/mm_dd2.jl")
 include("mm_approx/mm_dd4.jl")
+include("TO_simple.jl/transferoperator.jl")
 
 export markovmatrix,
     markovmatrixp,
@@ -32,9 +38,12 @@ export markovmatrix,
     mm_dd3,
     mm_dd2,
     mm_dd3,
-    mm_dd4
+    mm_dd4,
+    TO_from_binning
 
-t = triang_from_embedding(Embedding(invariant_gaussian_embedding(npts = 14, cov = 0.5, tau = 1)))
+emb = invariant_gaussian_embedding(npts = 17, cov = 0.5, tau = 1)
+t = triang_from_embedding(Embedding(emb))
 mm_dd4(t)
-
+n_bins = 10
+TO_from_binning(emb, n_bins)
 end # module
